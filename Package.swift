bcdaebf91be93bbb744ec5b940fb09854893a7d7
Claudio Gomez <claudio.gomez@redbull.com>
2023-08-22 12:09:13 -0600

    Added GoogleIMA SDKs for iOS version 3.19.2 and tvOS version 4.8.2.

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "GoogleInteractiveMediaAds",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "GoogleInteractiveMediaAds",
+            targets: ["GoogleIMA"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GoogleIMA-tvOS",
+            url: "https://imasdk.googleapis.com/native/downloads/ima-tvos-v4.8.2.zip",
+            checksum: "4e5702710cee997e7f1abf6393978eb9575c54b468edc5276b9a4d4d4879cb49"
+        ),
+        .binaryTarget(
+            name: "GoogleIMA-iOS",
+            url: "https://imasdk.googleapis.com/native/downloads/ima-ios-v3.19.2.zip",
+            checksum: "779ad89d07c343ae4530335159a1094ce6a69300cb06a47a92f65c4e8b1b80f1"
+        ),
+        .target(
+            name: "GoogleIMA",
+            dependencies: [
+                .target(name: "GoogleIMA-iOS", condition: .when(platforms:[.iOS])),
+                .target(name: "GoogleIMA-tvOS", condition: .when(platforms: [.tvOS]))
+            ]
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rbtv-google-ima
-Contains the framework Google Interactive Media Ads
+Contains the framework Google Interactive Media Ads for both iOS and tvOS.
+To be able to update this repository, please search the Podspec file for the version you want to update for GoogleIMA library (either iOS or tvOS) and copy the url where to download the zip and paste it in the Package manifest.
+Then download the zip file, and run the command: `swift package compute-checksum name-of-the-zip.zip`, the value returned must be copied and pasted to the checksum property in the Package manifest.
+Since each framework has its own version, this repository will start from `1.0.0`, using the latest version the app was using when we were using Cocoapods.
